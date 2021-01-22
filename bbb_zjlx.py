412804404759f41d17a064959a5751006e350081
itimor <itimor@126.com>
2021-01-22 22:26:52 +0800

    add

--- bbb_zjlx.py
+++ bbb_zjlx.py
@@ -15,12 +15,11 @@ import os
 ua = UserAgent()
 headers = {'User-Agent': ua.random}
 
-
 def get_stocks():
-    num = 1800
+    num = 2000
     timestamp = datetime.timestamp(dd)
     t1 = int(timestamp * 1000)
-    url = f'http://push2.eastmoney.com/api/qt/clist/get?cb=jQuery112304319747659550317_1610554619377&fid=f3&po=1&pz={num}&pn=1&np=1&fltt=2&invt=2&ut=b2884a393a59ad64002292a3e90d46a5&fs=m%3A0%2Bt%3A6%2Bf%3A!2%2Cm%3A0%2Bt%3A13%2Bf%3A!2%2Cm%3A0%2Bt%3A80%2Bf%3A!2%2Cm%3A1%2Bt%3A2%2Bf%3A!2%2Cm%3A1%2Bt%3A23%2Bf%3A!2%2Cm%3A0%2Bt%3A7%2Bf%3A!2%2Cm%3A1%2Bt%3A3%2Bf%3A!2&fields=f12%2Cf14%2Cf2%2Cf3%2Cf62%2Cf184%2Cf66%2Cf69%2Cf72%2Cf75%2Cf78%2Cf81%2Cf84%2Cf87%2Cf204%2Cf205%2Cf124'
+    url = f'http://push2.eastmoney.com/api/qt/clist/get?cb=jQuery1123003212074373104312_1611325176819&fid=f184&po=1&pz={num}&pn=1&np=1&fltt=2&invt=2&ut=b2884a393a59ad64002292a3e90d46a5&fs=m%3A0%2Bt%3A6%2Bf%3A!2%2Cm%3A0%2Bt%3A13%2Bf%3A!2%2Cm%3A0%2Bt%3A80%2Bf%3A!2%2Cm%3A1%2Bt%3A2%2Bf%3A!2%2Cm%3A1%2Bt%3A23%2Bf%3A!2%2Cm%3A0%2Bt%3A7%2Bf%3A!2%2Cm%3A1%2Bt%3A3%2Bf%3A!2&fields=f12%2Cf14%2Cf2%2Cf3%2Cf62%2Cf184%2Cf66%2Cf69%2Cf72%2Cf75%2Cf78%2Cf81%2Cf84%2Cf87%2Cf204%2Cf205%2Cf124'
     r = requests.get(url, headers=headers).text
     X = re.split('}}', r)[0]
     X = re.split('"diff":', X)[1]
