a606a83398e4f0e63ef0d8255630746db8149387
itimor <itimor@126.com>
2021-01-25 21:12:33 +0800

    add 机构调研

---
+++ ddd_zjlx.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+# author: itimor
+# 东方财富机构调研
+
+from datetime import datetime, timedelta
+from fake_useragent import UserAgent
+from sqlalchemy import create_engine
+import pandas as pd
+import numpy as np
+import tushare as ts
+import re
+import requests
+import os
+import json
+
+ua = UserAgent()
+headers = {'User-Agent': ua.random}
+
+
+def get_stocks():
+    num = 288
+    url = f'http://datainterface3.eastmoney.com/EM_DataCenter_V3/api/JGDYHZ/GetJGDYMX?js=datatable2799830&tkn=eastmoney&secuCode=&sortfield=0&sortdirec=1&pageNum=1&pageSize={num}&cfg=jgdyhz&p=16&pageNo=16&_=1611576606243'
+    r = requests.get(url, headers=headers).text
+    X = re.split('}]}', r)[0]
+    X = re.split('Close","Data":', X)[1]
+    s_data = eval(X)
+    s_dict = dict()
+    for s in s_data:
+        a = s.split('|')
+        if a[5] not in s_dict:
+            s_dict[a[5]] = {
+                'pre_code': a[5],
+                'name': a[6],
+                'close': float(a[17]),
+                'return': float(a[18]),
+                'num': int(a[4]),
+                'sum': int(a[4]),
+                'start_day': a[7],
+                'end_day': a[7],
+            }
+        else:
+            s_dict[a[5]]['sum'] += int(a[4])
+            s_dict[a[5]]['start_day'] = a[7]
+
+    df = pd.DataFrame(s_dict.values())
+    df['code'] = str(df['pre_code'])
+    s_codes = []
+    for i in df['pre_code']:
+        if len(str(i)) < 6:
+            s = '0' * (6 - len(str(i))) + str(i)
+        else:
+            s = str(i)
+        if s[0] == '6':
+            s = s + '.SH'
+        else:
+            s = s + '.SZ'
+        if len(s_codes) == 0:
+            s_codes = [s]
+        else:
+            s_codes.append(s)
+    df['code'] = s_codes
+    df.drop(['pre_code'], axis=1, inplace=True)
+    dfs = df[~ df['name'].str.contains('ST')]
+    last_dfs = dfs[~ dfs['code'].str.contains('^300|^688|^900')]
+    return last_dfs
+
+
+def main():
+    dfs = get_stocks()
+    columns = ['code', 'name', 'close', 'return', 'num', 'sum', 'start_day', 'end_day']
+    table = f'b_new'
+    df = dfs.loc[
+        (dfs["sum"] > 7) &
+        (dfs["close"] < 50), columns]
+    print(df[:5])
+    df.to_sql(table, con=engine, index=False, if_exists='replace')
+
+
+if __name__ == '__main__':
+    db = 'ddd'
+    d_format = '%Y%m%d'
+    t_format = '%H%M'
+    # 获得当天
+    dd = datetime.now()
+    cur_d = dd.strftime(d_format)
+    cur_t = dd.strftime(t_format)
+    if dd.hour > 15:
+        # ts初始化
+        ts_data = ts.pro_api('d256364e28603e69dc6362aefb8eab76613b704035ee97b555ac79ab')
+        df = ts_data.trade_cal(exchange='', start_date=cur_d, end_date=cur_d, is_open='1')
+        print(df)
+        if not os.path.exists(cur_d):
+            os.makedirs(cur_d)
+        if len(df) > 0:
+            # 创建连接引擎
+            engine = create_engine(f'sqlite:///{cur_d}/{db}.db', echo=False, encoding='utf-8')
+            main()
---
+++ ddd_zjlx_realtime.py
@@ -0,0 +1,105 @@
+# -*- coding: utf-8 -*-
+# author: itimor
+# 东方财富资金流向，并根据策略筛选股票
+
+from datetime import datetime, timedelta
+from telegram import Bot, ParseMode
+from fake_useragent import UserAgent
+from sqlalchemy import create_engine
+import pandas as pd
+import numpy as np
+import tushare as ts
+import requests
+import re
+import json
+
+ua = UserAgent()
+
+# 今日尾盘入  明日大涨 后天大大涨
+"""
+SELECT * from b_new where ogc <-2 and master > 10 and change < 6 ORDER by master;
+"""
+# 今日早盘入  明日大涨 后天大大涨
+"""
+SELECT * from b_new where ogc <0 and master < 20 and big > 0 and small < 0 and close < 10 ORDER by ogc;
+"""
+
+
+def get_stocks(codes):
+    dfs = []
+    for pre_code in codes:
+        print(pre_code)
+        c = pre_code.split('.')
+        code = f'{c[1].lower()}{c[0]}'
+        url = f'http://hq.sinajs.cn/list={code}'
+        headers = {'User-Agent': ua.random}
+        r = requests.get(url, headers=headers).text
+        X = re.split('";', r)[0]
+        X = re.split('="', X)[1]
+        d = X.split(',')
+        if d[-1] == '00':
+            d_data = {
+                'code': pre_code,
+                'open': d[1],
+                'now': d[3],
+                'change': (float(d[3]) - float(d[1])) / (float(d[1]) + 0.0001) * 100,
+                'ogc': (float(d[1]) - float(d[2])) / (float(d[2]) + 0.0001) * 100,
+            }
+        else:
+            d_data = {
+                'code': pre_code,
+                'open': d[2],
+                'now': d[2],
+                'change': 0.0,
+                'ogc': 0.0,
+            }
+        dfs.append(d_data)
+    dfs_json = json.dumps(dfs)
+    df_a = pd.read_json(dfs_json, orient='records')
+    return df_a
+
+
+def send_tg(text, chat_id):
+    token = '723532221:AAH8SSfM7SfTe4HmhV72QdLbOUW3akphUL8'
+    bot = Bot(token=token)
+    chat_id = chat_id
+    bot.send_message(chat_id=chat_id, text=text, parse_mode=ParseMode.HTML)
+
+
+def main(date, s_table):
+    df = pd.read_sql_query(f'select * from {s_table}', con=engine)
+    dfs = get_stocks(df['code'].to_list())
+    if len(dfs) > 0:
+        new_df = pd.merge(df, dfs, how='inner', left_on=['code'], right_on=['code'])
+        cur_t = '1600'
+        if dd.hour > 15:
+            cur_t = '1600'
+
+        d_table = f'{table_type}_new_{cur_t}'
+        df_a = new_df.sort_values(by=['ogc'], ascending=True)
+        df_a.to_sql(d_table, con=engine, index=False, if_exists='replace')
+        print(df_a.head())
+
+
+if __name__ == '__main__':
+    db = 'ddd'
+    d_format = '%Y%m%d'
+    t_format = '%H%M'
+    # 获得当天
+    dd = datetime.now()
+    start_date = dd - timedelta(days=10)
+    end_date = dd - timedelta(days=1)
+    cur_t = dd.strftime(t_format)
+    if dd.hour > 8:
+        # ts初始化
+        ts_data = ts.pro_api('d256364e28603e69dc6362aefb8eab76613b704035ee97b555ac79ab')
+        df = ts_data.trade_cal(exchange='', start_date=start_date.strftime(d_format),
+                               end_date=end_date.strftime(d_format), is_open='1')
+        last_d = df.tail(1)['cal_date'].to_list()[0]
+        # last_d = "20210116"
+        # 创建连接引擎
+        engine = create_engine(f'sqlite:///{last_d}/{db}.db', echo=False, encoding='utf-8')
+        table_type = 'b'
+        # table_type = 'c'
+        s_table = f'{table_type}_new'
+        main(last_d, s_table)
